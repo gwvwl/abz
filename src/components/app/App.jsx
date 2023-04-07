2fc1df03cf53e56955a6ebfba213a98888bfa3e8
Valerii <97701282+gwvwl@users.noreply.github.com>
2023-04-07 19:40:58 +0200

    wip

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
     <>
       <Header />
       <Users setRefUsers={setRefUsers} />
-      {submit ? <FormSingUp refUsers={refUsers} /> : <SuccessForm />}
+      {!submit ? <FormSingUp refUsers={refUsers} /> : <SuccessForm />}
     </>
   );
 };
